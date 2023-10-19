2c75b386fe25610af3321bc45adae5b556f6f2b2
vaishnavitatkare <109810298+vaishnavitatkare@users.noreply.github.com>
2023-10-19 20:48:22 +0530

    Add files via upload

---
+++ Exp4/blockchain.js
@@ -0,0 +1,25 @@
+
+const Block = require('./block');
+
+class Blockchain {
+
+    constructor() {
+        this.chain = [Block.genesis()];
+    }
+
+    addBlock({ data }) {
+        const newBlock = Block.mineBlock({
+            lastBlock: this.chain[this.chain.length-1],
+            data
+        });
+
+        this.chain.push(newBlock);
+    }
+}
+
+module.exports = Blockchain;/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
---
+++ Exp4/server.js
@@ -0,0 +1,12 @@
+
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+const blockchain = new Blockchain();
+
+for(let i=0; i<5; i++) {
+    const newData = 'Varun'+i;
+    blockchain.addBlock({data: newData});
+}
+
+console.log(blockchain);
